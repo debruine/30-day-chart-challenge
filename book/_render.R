8a478576844f088435c11a11b68f1684b2fdce3c
debruine <debruine@gmail.com>
2022-04-03 13:50:16 +0100

    added data to day3

--- book/_render.R
+++ book/_render.R
@@ -1,3 +1,10 @@
 # render the book as HTML and open in a browser
 xfun::in_dir("book", bookdown::render_book("index.Rmd", "bookdown::bs4_book"))
 browseURL("docs/index.html")
+
+# copy data directory to docs
+R.utils::copyDirectory(
+  from = "book/data",
+  to = "docs/data",
+  overwrite = TRUE,
+  recursive = TRUE)
